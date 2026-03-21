76e023e19a2bc96bac059252eabbdcc546f84dab
adrrp <adriarp03@gmail.com>
2026-03-22 00:07:01 +0100

    s

--- Week3/src/models/__init__.py
+++ Week3/src/models/__init__.py
@@ -1,3 +1,3 @@
-from .baseline import Baseline, Encoder, Decoder, char2idx, idx2char, chars, NUM_CHAR, TEXT_MAX_LEN
+from .baseline import Baseline, char2idx, idx2char, chars, NUM_CHAR, TEXT_MAX_LEN
 from .train_wrapper import CaptioningModule
 from .metrics import Metric
